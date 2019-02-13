bb885f7167fc6f5c47d4d4dc97878e8a5e411428
vagrant <vagrant@vagrant.vm>
2019-02-13 09:13:52 +0000

    Made login_form.html and register_form.html; Added routes in server.py to handle login and registration

--- server.py
+++ server.py
@@ -25,6 +25,71 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Show login form and register button """
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process login."""
+
+    # Get form variables
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("Looks like you have not yet registered! Please register")
+        return redirect("/register")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show form for user signup."""
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process registration."""
+
+    # Get form variables
+    email = request.form.get("email")
+    password = request.form.get("password")
+    name = request.form.get("name")
+    age = int(request.form.get("age"))
+    zipcode = request.form.get("zipcode")
+
+    # Add the new user to the database
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        new_user = User(email=email, password=password, name=name, 
+                        age=age, zipcode=zipcode)
+        db.session.add(new_user)
+        db.session.commit()
+    else:
+        flash("This email is already in use, please try logging in!")
+        redirect("/login")
+
+    flash(f"Thank you for registering, {name}")
+    return redirect("/login")
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
